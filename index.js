3be5787ce1a707c63329bc6e3044198adcade0ca
Sam Gluck <sdgluck@gmail.com>
2017-09-06 01:49:52 +0100

    extend node.assert

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+'use strict'
+
+const assert = require('assert')
+
 function n (name) {
   return name ? name + ' to be ' : ''
 }
@@ -11,24 +15,26 @@ function th (type, name, name2) {
   )
 }
 
-var arse = module.exports = {
+module.exports = assert
+
+Object.assign(assert, {
   ok: function (val, name) {
     if (!val) {
       throw th('ok', name)
     }
-    return arse
+    return assert
   },
   notOk: function (val, name) {
     if (val) {
       throw th('not ok', name)
     }
-    return arse
+    return assert
   },
   str: function (val, name) {
     if (typeof val !== 'string') {
       throw th('string', name)
     }
-    return arse
+    return assert
   },
   obj: function (val, name) {
     if (
@@ -37,66 +43,66 @@ var arse = module.exports = {
     ) {
       throw th('object', name)
     }
-    return arse
+    return assert
   },
   nonEmptyStr: function (val, name) {
     if (typeof val !== 'string' || !val.length) {
       throw th('non-empty string', name)
     }
-    return arse
+    return assert
   },
   num: function (val, name) {
     if (typeof val !== 'number') {
       throw th('number', name)
     }
-    return arse
+    return assert
   },
   sym: function (val, name) {
     if (typeof val !== 'symbol') {
       throw th('symbol', name)
     }
-    return arse
+    return assert
   },
   int: function (val, name) {
     if (typeof val !== 'number' || val % 1 !== 0) {
       throw th('integer', name)
     }
-    return arse
+    return assert
   },
   bool: function (val, name) {
     if (typeof val !== 'boolean') {
       throw th('boolean', name)
     }
-    return arse
+    return assert
   },
   undef: function (val, name) {
     if (typeof val !== 'undefined') {
       throw th('undefined', name)
     }
-    return arse
+    return assert
   },
   null: function (val, name) {
     if (val !== null) {
       throw th('null', name)
     }
-    return arse
+    return assert
   },
   elem: function (val, arr, name, name2) {
     if (arr.indexOf(val) === -1) {
       throw th('element of array', name, name2)
     }
-    return arse
+    return assert
   },
   key: function (val, obj, name, name2) {
     if (!(val in obj)) {
       throw th('key of object', name, name2)
     }
-    return arse
+    return assert
   },
   fn: function (val, name) {
     if (typeof val !== 'function') {
       throw th('function', name)
     }
-    return arse
+    return assert
   }
-}
+}) 
